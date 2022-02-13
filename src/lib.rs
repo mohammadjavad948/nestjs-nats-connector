508d956c8f322fe747df67f1b9c89a60e5123412
mohammadjavad <mjkmo123@gmail.com>
2022-02-13 20:50:46 +0330

    finally

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 mod listener {
+    use std::error::Error;
     use async_nats::{Connection, Message};
     use serde::{Deserialize, Serialize};
     use std::str;
+    use serde::de::DeserializeOwned;
 
     pub trait Listener {
-        type Pattern: Serialize;
+        type Pattern: Serialize + DeserializeOwned;
+        type RequestData: DeserializeOwned;
 
         fn handler(connection: &Connection, message: &Message);
 
@@ -14,8 +17,15 @@ mod listener {
             serde_json::to_string(&self.get_pattern()).unwrap()
         }
 
-        fn deserialize_message_data<Data>(&self, message: Message){
-            let stringify_data = str::from_utf8(&message.data).unwrap();
+        fn deserialize_message_data(&self, message: Message)
+            -> Result<
+                IncomingRequest<Self::Pattern, Self::RequestData>,
+                Box<dyn Error>
+            >
+        {
+            let stringify_data = str::from_utf8(&message.data)?;
+
+            Ok(serde_json::from_str(stringify_data).unwrap())
         }
     }
 
