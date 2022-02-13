6dd59aa5fd2bcaf149e2a5b8d53999aa64938c7e
mohammadjavad <mjkmo123@gmail.com>
2022-02-13 21:46:19 +0330

    will it work?

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@ mod listener {
                     .await
                     .unwrap();
 
-            futures.push(run_handler(T, listener));
+            futures.push(run_handler(T, listener, connection));
         }
 
         futures.collect::<Vec<()>>().await;
