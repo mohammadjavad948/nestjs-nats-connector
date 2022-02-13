118877ccf954bb33e1e6d27ad0100af7731eabf7
mohammadjavad <mjkmo123@gmail.com>
2022-02-13 21:58:53 +0330

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod listener {
-    use std::error::Error;
     use async_nats::{Connection, Message, Subscription};
-    use serde::{Deserialize, Serialize};
-    use std::str;
-    use serde::de::DeserializeOwned;
     use async_trait::async_trait;
     use futures::stream::FuturesUnordered;
     use futures::StreamExt;
+    use serde::de::DeserializeOwned;
+    use serde::{Deserialize, Serialize};
+    use std::error::Error;
+    use std::str;
 
     #[async_trait]
     pub trait Listener {
@@ -17,7 +17,7 @@ mod listener {
             &self,
             connection: &Connection,
             message: &Message,
-            data: IncomingRequest<Self::Pattern, Self::RequestData>
+            data: IncomingRequest<Self::Pattern, Self::RequestData>,
         );
 
         fn get_pattern(&self) -> Self::Pattern;
@@ -26,12 +26,10 @@ mod listener {
             serde_json::to_string(&self.get_pattern()).unwrap()
         }
 
-        fn deserialize_message_data(&self, message: &Message)
-            -> Result<
-                IncomingRequest<Self::Pattern, Self::RequestData>,
-                Box<dyn Error>
-            >
-        {
+        fn deserialize_message_data(
+            &self,
+            message: &Message,
+        ) -> Result<IncomingRequest<Self::Pattern, Self::RequestData>, Box<dyn Error>> {
             let stringify_data = str::from_utf8(&message.data)?;
 
             Ok(serde_json::from_str(stringify_data).unwrap())
@@ -45,17 +43,14 @@ mod listener {
         pub data: Data,
     }
 
-    pub async fn listen<T: Listener>(
-        connection: &Connection,
-        listeners: Vec<T>
-    ){
+    pub async fn listen<T: Listener>(connection: &Connection, listeners: Vec<T>) {
         let futures = FuturesUnordered::new();
 
         for list in listeners {
-            let listener =
-                connection.subscribe(&*list.serialize_pattern())
-                    .await
-                    .unwrap();
+            let listener = connection
+                .subscribe(&*list.serialize_pattern())
+                .await
+                .unwrap();
 
             futures.push(run_handler(list, listener, connection));
         }
@@ -63,18 +58,11 @@ mod listener {
         futures.collect::<Vec<()>>().await;
     }
 
-    async fn run_handler<T: Listener>(handler: T, listener: Subscription, connection: &Connection){
+    async fn run_handler<T: Listener>(handler: T, listener: Subscription, connection: &Connection) {
         for message in listener.next().await {
+            let deserialize = handler.deserialize_message_data(&message).unwrap();
 
-            let deserialize =
-                handler.deserialize_message_data(&message)
-                    .unwrap();
-
-            handler.handler(
-                connection,
-                &message,
-                deserialize
-            ).await;
+            handler.handler(connection, &message, deserialize).await;
         }
     }
 }
